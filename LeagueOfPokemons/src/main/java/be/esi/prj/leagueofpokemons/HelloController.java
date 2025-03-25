270dd02f334d8bd63c5f8d739c246ee358aa29c9
63491 <63491@etu.he2b.be>
2025-03-25 17:13:47 +0100

    Project creation

---
+++ LeagueOfPokemons/src/main/java/be/esi/prj/leagueofpokemons/HelloController.java
@@ -0,0 +1,14 @@
+package be.esi.prj.leagueofpokemons;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
