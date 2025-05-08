8fa7d48ff7a21a179a6e5b2f7a56aba3efbeb6a8
63491 <63491@etu.he2b.be>
2025-05-08 11:49:17 +0200

    module-info

--- LeagueOfPokemons/src/main/java/module-info.java
+++ LeagueOfPokemons/src/main/java/module-info.java
@@ -4,10 +4,10 @@ module be.esi.prj.leagueofpokemons {
     requires tess4j;
     requires java.net.http;
     requires com.fasterxml.jackson.databind;
-    requires java.desktop;
     requires java.sdk;
     requires javafx.media;
     requires java.sql;
+    requires javafx.swing;
 
 
     opens be.esi.prj.leagueofpokemons to javafx.fxml;
